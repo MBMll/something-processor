63e2a32cef5eb7d306447ece81684ccb559f4f13
lecong <aulor.xe@outlook.com>
2026-06-22 00:31:23 +0800

    add test case

---
+++ example-result-set-resolver/src/main/java/com/github/mbmll/sql/entity/Animal.java
@@ -0,0 +1,21 @@
+package com.github.mbmll.sql.entity;
+
+import com.github.mbmll.sql.annotation.AutoResultSetResolver;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Author xlc
+ * @Description
+ * @Date 2026/6/20 17:10
+ */
+@Data
+@AutoResultSetResolver
+public class Animal implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private Integer age;
+    private Boolean sex;
+    private String type;
+}
